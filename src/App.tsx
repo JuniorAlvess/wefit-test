8fbf2b3c97579e1722724f6c6475d5c670bbb8d5
Junior Alves <junioralves.jav@gmail.com>
2024-03-18 15:53:03 -0300

    feat: app configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import ErrorBoundary from './router/ErrorBoundary';
+import AppRoutes from './router/routes';
+
+import Header from './components/Header';
+
+import GlobalProvider from './context';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 1080px;
+  margin: 0 auto 24px;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 0 16px;
+  }
+`;
+
 function App() {
-  return <>WeMovies</>;
+  return (
+    <Router>
+      <ErrorBoundary>
+        <GlobalProvider>
+          <Container>
+            <Header />
+            <AppRoutes />
+          </Container>
+        </GlobalProvider>
+      </ErrorBoundary>
+    </Router>
+  );
 }
 
 export default App;
