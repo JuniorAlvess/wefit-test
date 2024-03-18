40d29f7a4d296e15b2d1e63de5fc77ac52d5b209
Junior Alves <junioralves.jav@gmail.com>
2024-03-18 19:50:10 -0300

    chore: dotenv added to the project

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-const baseURL = import.meta.env.API_BASE_URL_PROD || 'http://localhost:5173';
+const baseURL = import.meta.env.VITE_BASE_URL_PROD || 'http://localhost:5173';
 
 const api: AxiosInstance = axios.create({
   baseURL,
