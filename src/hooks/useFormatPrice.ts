ecb79d2c727725679eec2566e6724bdad8dc3c91
Junior Alves <junioralves.jav@gmail.com>
2024-03-25 12:40:28 -0300

    refactor: unnecessary code removed

--- src/hooks/useFormatPrice.ts
+++ src/hooks/useFormatPrice.ts
@@ -1,22 +1,5 @@
-import { useState, useEffect } from 'react';
-
 const useFormatPrice = (price: number) => {
-  const [formattedPrice, setFormattedPrice] = useState('');
-
-  useEffect(() => {
-    const formatPrice = (price: number) => {
-      if (typeof price !== 'number') {
-        console.error('Invalid price type. Price must be a number.');
-        return '';
-      }
-
-      return price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    };
-
-    setFormattedPrice(formatPrice(price));
-  }, [price]);
-
-  return formattedPrice;
+  return price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 };
 
 export default useFormatPrice;
